48967d6970b72a6828e0d28ff99b01bc56f82e4d
razvan-ritivoi <razvan.ritivoi@visma.com>
2023-02-03 15:50:53 +0200

    Add processing and fetching CV data functions

--- app.py
+++ app.py
@@ -37,6 +37,69 @@ class CV:
             page_text = page.extract_text().split("\n")[:-1]
             text += page_text
         return text
+    
+    def process_cv_data(self):
+        text = self.__import_CV()
+        for index, row in enumerate(text[2:], 2):
+            match = re.findall(r".+@.+\..+", row)
+            if match:
+                email = match[0]
+                continue
+            
+            match = re.findall(r"linkedin.com.+", row)
+            if match:
+                linkedin_url = match[0]
+            
+            if "Summary" in row:
+                index_summary = index
+            
+            elif "Experience" in row:
+                summary = text[index_summary + 1 : index]
+                summary = [row for row in summary if "\xa0" not in row]
+                
+                index_experience = index
+            
+            elif "Education" in row:
+                experience_row_list = text[index_experience + 1 : index]
+                nr_exp = len(experience_row_list)
+                experiences = [experience_row_list[i : i + 3] for i in range(0, nr_exp, 3)]
+                experiences = [
+                    dict(zip(["title", "company", "period"], exp))
+                    for exp in experiences
+                ]
+                
+                index_education = index
+                
+            elif "Licenses & Certifications" in row:
+                education_row_list = text[index_education + 1 : index]
+                nr_edu = len(education_row_list)
+                education = [education_row_list[i : i + 3] for i in range(0, nr_edu, 3)]
+                education = [
+                    dict(zip(["university", "degree", "period"], edu))
+                    for edu in education
+                ]
+                
+            elif "Skills" in row:
+                skills = text[index + 1 :]
+                skills = [skill.replace("\xa0", "") for skill in skills]
+                
+        cv_data = {
+            "personal": {
+                "name": f"{text[0]}",
+                "location": f"{text[1]}",
+                "email": email,
+                "linkedin": linkedin_url,
+                "summary": " ".join(summary),
+            },
+            "experience": {"companies": experiences, "skills": skills},
+            "education": education,
+        }
+        return cv_data
+    
+    def fetch_cv_data(self, resume_part):
+        if "all" in resume_part:
+            return self.cv_data
+        return self.cv_data[resume_part]
 
 
 @app.route("/", methods=["GET"])
